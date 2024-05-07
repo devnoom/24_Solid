68d5bad7d2b86d0a7181705d4047a6665312e43e
devnoom <archilmenabde@yahoo.com>
2024-05-08 01:13:14 +0400

    Initial Commit

---
+++ 24_Solid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  24_Solid
+//
+//  Created by MacBook Air on 08.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
